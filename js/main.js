4ba641d85567855b3d214b5a03b91df1b97b0fb1
quiesqueso <quiespan@gmail.com>
2025-11-21 13:45:42 +0100

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Cambiar header al hacer scroll
-const header = document.getElementById("header");
-
-
-window.addEventListener("scroll", () => {
-if (window.scrollY > 60) header.classList.add("scrolled");
-else header.classList.remove("scrolled");
-});
